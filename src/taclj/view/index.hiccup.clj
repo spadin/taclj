0bebc456ec54d56e039941bd6aeb5b42a46d3531
Sandro Padin <sandropadin@gmail.com>
2013-02-26 13:08:16 -0600

    Moving header out to layout

--- src/taclj/view/index.hiccup.clj
+++ src/taclj/view/index.hiccup.clj
@@ -1,24 +1,22 @@
-[:h1 "Taclj"
-  [:em [:small "- tic tac toe"]]]
+[:div {:id "start-game"}
+  [:h3 "Start a new game"]
 
-[:h3 "Start a new game"]
+  [:form {:action "/game" :id "new-game"}
+    [:label {:for "game-type"} "Choose game type"]
+    [:select {:name "game-type" :id "game-type"}
+      (for [game-type (get-game-types)]
+        [:option {:value (:uri-value game-type)} (:name game-type)])]
 
-[:form {:action "/game" :id "new-game"}
-  [:label {:for "game-type"} "Choose game type"]
-  [:select {:name "game-type" :id "game-type"}
-    (for [game-type (get-game-types)]
-      [:option {:value (:uri-value game-type)} (:name game-type)])]
+    [:label {:for "x-player"} "X Player Type"]
+    [:select {:name "x-player" :id "x-player"}
+      (for [player-type (get-player-types)]
+        [:option {:value (:uri-value player-type)} (:name player-type)])]
 
-  [:label {:for "x-player"} "X Player Type"]
-  [:select {:name "x-player" :id "x-player"}
-    (for [player-type (get-player-types)]
-      [:option {:value (:uri-value player-type)} (:name player-type)])]
+    [:label {:for "o-player"} "O Player Type"]
+    [:select {:name "o-player" :id "o-player"}
+      (for [player-type (get-player-types)]
+        [:option {:value (:uri-value player-type)} (:name player-type)])]
 
-  [:label {:for "o-player"} "O Player Type"]
-  [:select {:name "o-player" :id "o-player"}
-    (for [player-type (get-player-types)]
-      [:option {:value (:uri-value player-type)} (:name player-type)])]
-
-  [:input {:type "submit"
-           :value "Start game"
-           :class "submit"}]]
+    [:input {:type "submit"
+             :value "Start game"
+             :class "submit"}]]]
--- src/taclj/view/layout.hiccup.clj
+++ src/taclj/view/layout.hiccup.clj
@@ -6,5 +6,8 @@
   (include-css "/stylesheets/taclj.css")
   (include-js "/javascript/taclj.js")]
  [:body
+  [:h1 "Taclj"
+    [:em [:small "- tic tac toe"]]]
+
   (eval (:template-body joodo.views/*view-context*))
 ]]
