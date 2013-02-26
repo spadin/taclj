5542f65d16b6cdad5de45267f8c7f167918718d9
Sandro Padin <sandropadin@gmail.com>
2013-02-26 11:42:13 -0600

    Adding joodo/lein-joodo plugin to project

--- project.clj
+++ project.clj
@@ -9,5 +9,6 @@
   :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
   :test-paths ["spec/"]
   :java-source-paths ["src/"]
-  :plugins [[speclj "2.5.0"]]
+  :plugins [[speclj "2.5.0"]
+            [joodo/lein-joodo "1.1.2"]]
   :min-lein-version "2.0.0")
